8d6bb5d43fc1e8bd94aec0eb0852811e7de86d58
BartHagoort <bart.hagoort@hotmail.nl>
2021-09-29 09:57:57 +0200

    Added BackEnd Project

---
+++ Backend/Plansysteem_BackEnd_ApI/Plansysteem_BackEnd_DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Plansysteem_BackEnd_DAL
+{
+    public class Class1
+    {
+    }
+}
---
+++ Backend/Plansysteem_BackEnd_ApI/Plansysteem_BackEnd_Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Plansysteem_BackEnd_Logic
+{
+    public class Class1
+    {
+    }
+}
